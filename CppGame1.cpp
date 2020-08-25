3b816fdb271fde013f81495cf9c74b23fe664f47
Aaron Smith <aaron.smith@sgws.com>
2020-08-25 17:15:16 -0500

    learning cpp

--- CppGame1.cpp
+++ CppGame1.cpp
@@ -2,5 +2,23 @@
 
 int main()
 {
+    std::cout << "You are a computer hacker that has to break into secure files...";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct code to access the files..." << std::endl;
+
+    const int CodeA = 4;
+    const int CodeB = 2;
+    const int CodeC = 6;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    std::cout << std::endl;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to: " << CodeProduct << std::endl;
+
+    int PlayerGuess;
+
     return 0;
 }
