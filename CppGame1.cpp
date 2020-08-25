1da234a3f60fed644e492334776ae40fee707971
Aaron Smith <aaron.smith@sgws.com>
2020-08-25 18:49:52 -0500

    created seperate functions learning syntax

--- CppGame1.cpp
+++ CppGame1.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 
-int main()
+void PrintIntro()
 {
-    std::cout << "You are a computer hacker that has to break into secure files...";
-    std::cout << std::endl;
-    std::cout << "You need to enter the correct code to access the files..." << std::endl;
+    //print welcome and game intro to terminla
+    std::cout << "You are a computer hacker that has to break into secure files...\n";
+    std::cout << "You need to enter the correct code to access the files...\n\n";
+}
 
+void PlayGame()
+{
+    PrintIntro();
+    //set variables
     const int CodeA = 4;
     const int CodeB = 2;
     const int CodeC = 6;
@@ -13,12 +18,28 @@ int main()
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiply to: " << CodeProduct << std::endl;
+    //print codesum and codeproduct to terminal
+    std::cout << "+ There are 3 numbers in the code";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to: " << CodeProduct << "\n";
 
-    int PlayerGuess;
+    //store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    //check if player guess is correct
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
+        std::cout << "You Hacked In!!!";
+    } else {
+        std::cout << "The computer exploded destroying the files!!!";
+    }
+}
+
+int main()
+{
+    PlayGame();
     return 0;
 }
