2bd5e596e8f7a210bf1ba4d8cd8e5ad92976880f
Aaron Smith <aaron.smith@sgws.com>
2020-08-26 16:14:41 -0500

    working cpp terminal hacker game

--- CppGame1.cpp
+++ CppGame1.cpp
@@ -1,45 +1,58 @@
 #include <iostream>
+#include <ctime>
 
-void PrintIntro()
-{
-    //print welcome and game intro to terminla
-    std::cout << "You are a computer hacker that has to break into secure files...\n";
-    std::cout << "You need to enter the correct code to access the files...\n\n";
+void PrintIntro (int Difficulty) {
+    std::cout << "\n\nYou are a computer hacker that has to break into level " << Difficulty << " security files...\n";
+    std::cout << "You need to enter the correct three digit code to access the files...\n";
+    std::cout << "To enter code seperate the three numbers with spaces...\n\n";
 }
 
-void PlayGame()
-{
-    PrintIntro();
-    //set variables
-    const int CodeA = 4;
-    const int CodeB = 2;
-    const int CodeC = 6;
+bool PlayGame (int Difficulty) {
+
+    PrintIntro (Difficulty); 
+
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
-    //print codesum and codeproduct to terminal
     std::cout << "+ There are 3 numbers in the code";
     std::cout << "\n+ The codes add-up to: " << CodeSum;
     std::cout << "\n+ The codes multiply to: " << CodeProduct << "\n";
 
-    //store player guess
     int GuessA, GuessB, GuessC;
     std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
-    //check if player guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
-        std::cout << "You Hacked In!!!";
+        std::cout << "You Hacked In!!! Try to hack into harder files!";
+        return true;
     } else {
-        std::cout << "The computer exploded destroying the files!!!";
+        std::cout << "The computer exploded destroying the files!!! Be careful and try again.";
+        return false;
     }
 }
 
-int main()
-{
-    PlayGame();
-    return 0;
+int main() {
+    
+    srand(time(NULL));
+
+    int LevelDifficulty = 1;
+    const int MaxDifficulty = 5;
+
+    while (LevelDifficulty <= MaxDifficulty) {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
+
+        if (bLevelComplete) {
+            ++LevelDifficulty;
+        }
+    }
+    std::cout << "\nGood work hacker! You recovered the files now run!";
+    return 0; 
 }
